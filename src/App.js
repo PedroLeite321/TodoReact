201b234a931118240d8297a6d2449089d30d7503
Pedro Kauã <pk.bleite@gmail.com>
2023-07-02 01:50:14 -0300

    create, delete, undo and complete are done

--- src/App.js
+++ src/App.js
@@ -15,27 +15,47 @@ function App() {
     let obj = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id+ + 1,
       name: newText,
-      complete: false,
+      completed: false,
       
     };
     setTodoTask([...todoList, obj]);
     
   }
   
-  const deleteTask = () =>  {
-
+  const deleteTask = (args) =>  {
+    setTodoTask(todoList.filter((toDelete) => {
+      if(toDelete.id === args){
+        return false;
+      }else {
+        return toDelete;
+      }
+    }));
   }
 
-  const updateTask = () =>  {
-
+  const updateTask = (args) =>  {
+   
   }
   
-  const completedTask = () => {
-
+  const completedTask = ( args ) => {
+    //map function to create a copy with changes in its objects.
+    setTodoTask(todoList.map((completedTask) => {
+      if(completedTask.id === args) {
+        return{...completedTask, completed: true};
+      }
+      else{
+        return(completedTask);
+      }
+    }))
   }
 
-  const undo_Complete = () => {
-
+  const undo_Complete = (args) => {
+    setTodoTask(todoList.map((undo) => {
+      if(undo.id === args)  {
+        return {...undo, completed: false};
+      }else{
+        return undo;
+      }
+    }));
   }
 
   return (
@@ -46,7 +66,7 @@ function App() {
         <button onClick={createTask}>Add Item</button>
       </div>
       <div className="App">
-          {todoList.map((allTasks) => <Task name={allTasks.name} id={allTasks.id} delete={deleteTask} update={updateTask} undo={undo_Complete} complete={completedTask}/>)}
+          {todoList.map((allTasks) => <Task name={allTasks.name} id={allTasks.id} delete={deleteTask} update={updateTask} undo={undo_Complete} complete={completedTask} completed={allTasks.completed}/>)}
       </div>
     </main>
   );
--- src/task.js
+++ src/task.js
@@ -2,12 +2,13 @@
 const Task = (task) =>  {
 
     return(
-        <div style={{backgroundColor: task.completed ? "green" : "white"}} className="App">
+        <div style={{backgroundColor : task.completed === true? "green" : "red"}}  className="App">
             <h1>{task.name}</h1> 
-            <button onClick={task.complete(task.id)}>complete</button>
-            <button onClick={task.delete(task.id)}>Delete</button>
-            <button onClick={task.undo(task.id)}>undo</button>
-            <button onClick={task.update(task.id)}>update</button>
+            <button onClick={() => task.complete(task.id)}>Complete</button>
+            <button onClick={() => task.delete(task.id)}>Delete</button>
+            <button onClick={() => task.undo(task.id)}>Undo</button>
+            <button onClick={() => task.update(task.id)}>Update</button>
+           
         </div>
     );
 
